db25560d2daf86e65642ca8ca59fe45b05bf311e
jseibert <jseibert@css.edu>
2018-11-01 11:20:21 -0500

    Request 407: read trades

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -10,6 +10,22 @@ namespace SingleResponsibilityPrinciple
 {
     public class TradeProcessor
     {
+        private IEnumerable<string> ReadURLTradeData(string url)
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+        }
+
         private IEnumerable<string> ReadTradeData(Stream stream)
         {
             var tradeData = new List<string>();
@@ -141,6 +157,13 @@ namespace SingleResponsibilityPrinciple
             }
 
             LogMessage("INFO", "  {0} trades processed", trades.Count());
+        }
+
+        public void ProcessTrades(string url)
+        {
+            var lines = ReadURLTradeData(url);
+            var trades = ParseTrades(lines);
+            StoreTrades(trades);
         }
 
         public void ProcessTrades(Stream stream)
